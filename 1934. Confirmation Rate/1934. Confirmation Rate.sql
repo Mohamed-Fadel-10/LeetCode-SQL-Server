b625d9642019015e69d9f639c6d245ee45193bf4
Mohamed Fadel <fadelmohamed445@gmail.com>
2024-09-02 16:31:58 +0300

    1934. Confirmation Rate

---
+++ 1934. Confirmation Rate/1934. Confirmation Rate.sql
@@ -0,0 +1,16 @@
+-- Using SQL Server We Will Need To Cast Result Fisrt
+
+select s.user_id ,isnull(Round(cast(count(case when c.action='confirmed ' then 1 end ) as decimal(18,2)) / cast(count(*) as decimal(18,2)),2),0)as confirmation_rate 
+from Signups s left outer join Confirmations c
+on s.user_id =c.user_id 
+group by s.user_id 
+order by s.user_id  asc
+
+-- Using MySQL Server We Just Use The Round Without Cast 
+
+select s.user_id ,ifnull(Round(Count(case when c.action='confirmed ' then 1 end ) / count(case when c.user_id<>0 then c.user_id end),2) ,0)as confirmation_rate 
+from Signups s left outer join Confirmations c
+on s.user_id =c.user_id 
+group by s.user_id 
+
+
